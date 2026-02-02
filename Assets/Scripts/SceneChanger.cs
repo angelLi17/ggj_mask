262e3132b3f7e3ceea16a6f54e42d8413a670ec4
fleyc <81700317+fleyc@users.noreply.github.com>
2026-02-01 16:36:19 -0800

    edit

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
